5ecf3b6b90d2dbbaf07cfad41ad4e325af5a94c7
Dax Raad <d@ironbay.co>
2025-08-20 16:52:43 -0400

    ignore: cloud stuff

--- infra/app.ts
+++ infra/app.ts
@@ -25,9 +25,9 @@ export const api = new sst.cloudflare.Worker("Api", {
       ])
       args.migrations = {
         // Note: when releasing the next tag, make sure all stages use tag v2
-        oldTag: $app.stage === "production" ? "" : "v1",
-        newTag: $app.stage === "production" ? "" : "v1",
-        //newSqliteClasses: ["SyncServer"],
+        // oldTag: $app.stage === "production" ? "" : "v1",
+        // newTag: $app.stage === "production" ? "" : "v1",
+        newSqliteClasses: ["SyncServer"],
       }
     },
   },
