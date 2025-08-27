4732c42be9c44d8657959342bbc957aa5f5888e1
Jay V <air@live.ca>
2025-08-27 17:53:04 -0400

    ignore: cloud

--- cloud/app/src/entry-server.tsx
+++ cloud/app/src/entry-server.tsx
@@ -8,7 +8,9 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.svg" />
+          <meta property="og:image" content="/social-share.png" />
+          <meta property="twitter:image" content="/social-share.png" />
           {assets}
         </head>
         <body>
