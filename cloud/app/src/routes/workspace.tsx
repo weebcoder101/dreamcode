80e58299f22628d41ac3392f64c00905b8be6133
Jay V <air@live.ca>
2025-08-28 17:34:55 -0400

    ignore: cloud

--- cloud/app/src/routes/workspace.tsx
+++ cloud/app/src/routes/workspace.tsx
@@ -1,10 +1,11 @@
+import "./workspace/workspace.css"
 import { RouteSectionProps } from "@solidjs/router";
 
 export default function WorkspaceLayout(props: RouteSectionProps) {
   return (
-    <div>
+    <main data-page="workspace">
       <h1>Workspace</h1>
       {props.children}
-    </div>
+    </main>
   );
 }
