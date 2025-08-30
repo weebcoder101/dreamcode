17cb0b000c04c6d3bc510fd069f4804d378d6b63
Dax Raad <d@ironbay.co>
2025-08-30 01:36:25 -0400

    ignore: sync

--- cloud/app/src/routes/workspace.tsx
+++ cloud/app/src/routes/workspace.tsx
@@ -1,6 +1,20 @@
+import { useAuthSession } from "~/context/auth.session"
 import { IconLogo } from "../component/icon"
-import "./workspace/workspace.css"
-import { RouteSectionProps } from "@solidjs/router"
+import "./workspace.css"
+import { action, redirect, RouteSectionProps } from "@solidjs/router"
+
+const logout = action(async () => {
+  "use server"
+  const auth = await useAuthSession()
+  const current = auth.data.current
+  if (current)
+    await auth.update((val) => {
+      delete val.account[current]
+      return val
+    })
+
+  return redirect("/")
+})
 
 export default function WorkspaceLayout(props: RouteSectionProps) {
   return (
@@ -13,7 +27,9 @@ export default function WorkspaceLayout(props: RouteSectionProps) {
         </div>
         <div data-slot="header-actions">
           <span>name@example.com</span>
-          <a href="/logout">Logout</a>
+          <form action={logout} method="post">
+            <button type="submit" formaction={logout}>Logout</button>
+          </form>
         </div>
       </header>
       <div data-slot="content">{props.children}</div>
