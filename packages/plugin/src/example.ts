e9a3ee0f3df0ecfe3363bff7cfcd082c36fc0bb3
Dax Raad <d@ironbay.co>
2025-09-01 17:35:48 -0400

    docs: sdk

--- packages/plugin/src/example.ts
+++ packages/plugin/src/example.ts
@@ -1,9 +1,15 @@
 import { Plugin } from "./index"
 
-export const ExamplePlugin: Plugin = async ({ client, $ }) => {
+export const ExamplePlugin: Plugin = async ({
+  client: _client,
+  $: _shell,
+  project: _project,
+  directory: _directory,
+  worktree: _worktree,
+}) => {
   return {
     permission: {},
-    async "chat.params"(input, output) {
+    async "chat.params"(_input, output) {
       output.topP = 1
     },
   }
