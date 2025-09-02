8021b97b26f2bd76aa5a45f16ed6909dc71a906d
Dax Raad <d@ironbay.co>
2025-09-02 16:38:26 -0400

    wip: zen

--- cloud/app/src/context/auth.withActor.ts
+++ cloud/app/src/context/auth.withActor.ts
@@ -1,16 +1,7 @@
 import { Actor } from "@opencode/cloud-core/actor.js"
 import { getActor } from "./auth"
-import { query } from "@solidjs/router"
 
 export async function withActor<T>(fn: () => T) {
   const actor = await getActor()
   return Actor.provide(actor.type, actor.properties, fn)
-}
-
-export function actorQuery<T>(cb: () => T, name: string) {
-  "use server"
-  return query(async () => {
-    const actor = await getActor()
-    return withActor(cb)
-  }, name)
 }
