1a4ec54c8905fcd05d36b12206362394e539f751
Dax Raad <d@ironbay.co>
2025-09-16 13:49:37 -0400

    disable thinking for opencode zen and gpt-5 temporarily

--- packages/opencode/src/provider/transform.ts
+++ packages/opencode/src/provider/transform.ts
@@ -98,8 +98,8 @@ export namespace ProviderTransform {
       }
       if (providerID === "opencode") {
         result["promptCacheKey"] = sessionID
-        result["include"] = ["reasoning.encrypted_content"]
-        result["reasoningSummary"] = "detailed"
+        // result["include"] = ["reasoning.encrypted_content"]
+        // result["reasoningSummary"] = "detailed"
       }
     }
     return result
