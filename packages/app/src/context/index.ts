9b2b606f526e65a71a6127cb4ee17942b908820b
Adam <2363879+adamdotdevin@users.noreply.github.com>
2025-09-23 11:41:11 -0500

    wip: desktop file updates

--- packages/app/src/context/index.ts
+++ packages/app/src/context/index.ts
@@ -1,4 +1,7 @@
+export { EventProvider, useEvent } from "./event"
 export { LocalProvider, useLocal } from "./local"
+export { MarkedProvider, useMarked } from "./marked"
 export { SDKProvider, useSDK } from "./sdk"
+export { ShikiProvider, useShiki } from "./shiki"
 export { SyncProvider, useSync } from "./sync"
 export { ThemeProvider, useTheme } from "./theme"
