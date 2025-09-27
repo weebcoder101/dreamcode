f2f5fd8718fb354a854cfaa2f7907308176fda3c
Dax Raad <d@ironbay.co>
2025-09-27 04:10:56 -0400

    sync

--- packages/plugin/script/publish.ts
+++ packages/plugin/script/publish.ts
@@ -5,14 +5,24 @@ process.chdir(dir)
 
 import { $ } from "bun"
 
-const snapshot = process.env["OPENCODE_SNAPSHOT"] === "true"
-
 await $`bun tsc`
 
+const pkg = await import("../package.json")
+for (const [key, value] of Object.entries(pkg.exports)) {
+  const file = value.replace("./src/", "./").replace(".ts", "")
+  // @ts-expect-error
+  pkg.exports[key] = {
+    import: file + ".js",
+    types: file + ".d.ts",
+  }
+}
+await Bun.write("./dist/package.json", JSON.stringify(pkg, null, 2))
+
+const snapshot = process.env["OPENCODE_SNAPSHOT"] === "true"
+
 if (snapshot) {
-  await $`bun publish --tag snapshot --access public`
-  await $`git checkout package.json`
+  await $`bun publish --tag snapshot --access public`.cwd("./dist")
 }
 if (!snapshot) {
-  await $`bun publish --access public`
+  await $`bun publish --access public`.cwd("./dist")
 }
