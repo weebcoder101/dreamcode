2504c2f2664ffce2a707e2508939fb10ad1426e6
Adam <2363879+adamdotdevin@users.noreply.github.com>
2025-10-03 09:27:12 -0500

    chore: user-agent header

--- packages/console/app/src/lib/github.ts
+++ packages/console/app/src/lib/github.ts
@@ -2,21 +2,25 @@ import { query } from "@solidjs/router"
 
 export const github = query(async () => {
   "use server"
+  const headers = {
+    UserAgent:
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+  }
   try {
     const [meta, releases, contributors] = await Promise.all([
-      fetch("https://api.github.com/repos/sst/opencode").then(async (res) => {
+      fetch("https://api.github.com/repos/sst/opencode", { headers }).then(async (res) => {
         const text = await res.text()
         console.log(text)
         const json = JSON.parse(text)
         return json
       }),
-      fetch("https://api.github.com/repos/sst/opencode/releases").then(async (res) => {
+      fetch("https://api.github.com/repos/sst/opencode/releases", { headers }).then(async (res) => {
         const text = await res.text()
         console.log(text)
         const json = JSON.parse(text)
         return json
       }),
-      fetch("https://api.github.com/repos/sst/opencode/contributors?per_page=1"),
+      fetch("https://api.github.com/repos/sst/opencode/contributors?per_page=1", { headers }),
     ])
     const [release] = releases
     const contributorCount = Number.parseInt(
