401ef4832eb782b65287acbfc0d8bb005687f30a
Adam <2363879+adamdotdevin@users.noreply.github.com>
2025-10-03 09:30:51 -0500

    fix: user-agent

--- packages/console/app/src/lib/github.ts
+++ packages/console/app/src/lib/github.ts
@@ -3,7 +3,7 @@ import { query } from "@solidjs/router"
 export const github = query(async () => {
   "use server"
   const headers = {
-    UserAgent:
+    "User-Agent":
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
   }
   try {
