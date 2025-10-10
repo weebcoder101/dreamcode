77edc9142cddb9d959a4db62d0b4a7bbd50c4a44
Frank <frank@sst.dev>
2025-10-10 09:03:49 -0400

    wip: zen

--- packages/console/app/src/routes/workspace/[id]/index.tsx
+++ packages/console/app/src/routes/workspace/[id]/index.tsx
@@ -4,8 +4,14 @@ import { UsageSection } from "./usage-section"
 import { ModelSection } from "./model-section"
 import { ProviderSection } from "./provider-section"
 import { IconLogo } from "~/component/icon"
+import { createAsync, useParams } from "@solidjs/router"
+import { querySessionInfo } from "../common"
+import { Show } from "solid-js"
 
 export default function () {
+  const params = useParams()
+  const userInfo = createAsync(() => querySessionInfo(params.id))
+
   return (
     <div data-page="workspace-[id]">
       <section data-component="title-section">
@@ -22,7 +28,9 @@ export default function () {
       <div data-slot="sections">
         <NewUserSection />
         <ModelSection />
-        <ProviderSection />
+        <Show when={userInfo()?.isAdmin}>
+          <ProviderSection />
+        </Show>
         <UsageSection />
       </div>
     </div>
