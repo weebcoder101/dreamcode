5a8d45a6c1fcc63dabfda0f9c29edf1cc231929e
Haris Gušić <harisgusic.dev@gmail.com>
2025-10-17 21:34:37 +0200

    Improve typo directory error (#3247)

--- packages/opencode/src/cli/error.ts
+++ packages/opencode/src/cli/error.ts
@@ -10,8 +10,9 @@ export function FormatError(input: unknown) {
       `Config file at ${input.data.path} is not valid JSON(C)` + (input.data.message ? `: ${input.data.message}` : "")
     )
   }
-  if (Config.DirectoryError.isInstance(input))
-    return `Directory "${input.data.dir}" in ${input.data.path} is not valid. Did you mean "${input.data.suggestion}"?`
+  if (Config.ConfigDirectoryTypoError.isInstance(input)) {
+    return `Directory "${input.data.dir}" in ${input.data.path} is not valid. Use "${input.data.suggestion}" instead. This is a common typo.`
+  }
   if (Config.InvalidError.isInstance(input))
     return [
       `Config file at ${input.data.path} is invalid` + (input.data.message ? `: ${input.data.message}` : ""),
