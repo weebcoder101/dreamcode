2c933a00c0ea041f3be5ce6ffcceb32c91082d5c
Aiden Cline <aidenpcline@gmail.com>
2025-10-22 11:28:06 -0500

    tweak

--- packages/opencode/src/provider/provider.ts
+++ packages/opencode/src/provider/provider.ts
@@ -510,14 +510,7 @@ export namespace Provider {
 
     const provider = await state().then((state) => state.providers[providerID])
     if (!provider) return
-    const priority = [
-      "claude-haiku-4-5",
-      "anthropic/claude-haiku-4.5",
-      "3-5-haiku",
-      "3.5-haiku",
-      "gemini-2.5-flash",
-      "gpt-5-nano",
-    ]
+    const priority = ["claude-haiku-4-5", "3-5-haiku", "3.5-haiku", "gemini-2.5-flash", "gpt-5-nano"]
     for (const item of priority) {
       for (const model of Object.keys(provider.info.models)) {
         if (model.includes(item)) return getModel(providerID, model)
