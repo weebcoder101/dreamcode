3dc30cae503dc8492e77f292f99fd7b28ba02041
Aiden Cline <aidenpcline@gmail.com>
2025-11-07 15:32:08 -0600

    ignore: fix

--- script/publish.ts
+++ script/publish.ts
@@ -87,7 +87,7 @@ for (const file of pkgjsons) {
 const extensionToml = new URL("../packages/extensions/zed/extension.toml", import.meta.url).pathname
 let toml = await Bun.file(extensionToml).text()
 toml = toml.replace(/^version = "[^"]+"/m, `version = "${Script.version}"`)
-toml = toml.replaceAll(/releases\/v[^/]+\//g, `releases/v${Script.version}/`)
+toml = toml.replaceAll(/releases\/download\/v[^/]+\//g, `releases/download/v${Script.version}/`)
 console.log("updated:", extensionToml)
 await Bun.file(extensionToml).write(toml)
 
