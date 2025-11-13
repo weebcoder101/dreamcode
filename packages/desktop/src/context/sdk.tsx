67c9a07689680973a42b35260ba1d3341d41f4ae
Aiden Cline <aidenpcline@gmail.com>
2025-11-13 14:25:47 -0600

    fix: opencode run timeout

--- packages/desktop/src/context/sdk.tsx
+++ packages/desktop/src/context/sdk.tsx
@@ -10,11 +10,6 @@ export const { use: useSDK, provider: SDKProvider } = createSimpleContext({
     const sdk = createOpencodeClient({
       baseUrl: props.url,
       signal: abort.signal,
-      fetch: (req) => {
-        // @ts-ignore
-        req.timeout = false
-        return fetch(req)
-      },
     })
 
     const emitter = createGlobalEmitter<{
