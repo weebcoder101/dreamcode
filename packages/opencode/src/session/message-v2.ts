e43f8038726c8a80483c58cbd874c555075aed38
Aiden Cline <aidenpcline@gmail.com>
2025-11-20 14:13:56 -0600

    Revert "ignore: write todo"
    This reverts commit d46440fbadb78a6f8d46e5973b2c327ad07cc77d.

--- packages/opencode/src/session/message-v2.ts
+++ packages/opencode/src/session/message-v2.ts
@@ -638,8 +638,11 @@ export namespace MessageV2 {
                 state: "output-available",
                 toolCallId: part.callID,
                 input: part.state.input,
-                // TODO: prolly need something better here when dealing with synthetic user messages + attachments
-                output: part.state.time.compacted ? "[Old tool result content cleared]" : part.state.output,
+                output: part.state.attachments?.length
+                  ? "[Image content moved to user message]"
+                  : part.state.time.compacted
+                    ? "[Old tool result content cleared]"
+                    : part.state.output,
                 callProviderMetadata: part.metadata,
               })
             }
