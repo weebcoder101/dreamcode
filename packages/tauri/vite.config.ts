ec9c6775a02a444d05070a35abb45e6838b023c0
GitHub Action <action@github.com>
2025-11-22 05:15:55 +0000

    chore: format code

--- packages/tauri/vite.config.ts
+++ packages/tauri/vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vite"
 
 // @ts-expect-error process is a nodejs global
-const host = process.env.TAURI_DEV_HOST;
+const host = process.env.TAURI_DEV_HOST
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent Vite from obscuring rust errors
@@ -27,4 +26,4 @@ export default defineConfig(async () => ({
       ignored: ["**/src-tauri/**"],
     },
   },
-}));
+}))
