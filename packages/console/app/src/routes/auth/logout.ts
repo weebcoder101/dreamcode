78cec294e4975e5baf263219b388d38674391f21
Dax Raad <d@ironbay.co>
2025-11-23 00:12:58 -0500

    fix logout

---
+++ packages/console/app/src/routes/auth/logout.ts
@@ -0,0 +1,21 @@
+import { redirect } from "@solidjs/router"
+import { APIEvent } from "@solidjs/start"
+import { getResponseHeaders } from "@solidjs/start/http"
+import { useAuthSession } from "~/context/auth.session"
+
+export async function GET(event: APIEvent) {
+  const auth = await useAuthSession()
+  const current = auth.data.current
+  if (current)
+    await auth.update((val) => {
+      delete val.account?.[current]
+      const first = Object.keys(val.account ?? {})[0]
+      val.current = first
+      event!.locals.actor = undefined
+      return val
+    })
+  return redirect("/zen", {
+    status: 302,
+    headers: getResponseHeaders(),
+  })
+}
