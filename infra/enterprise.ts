94c27e75ed7f0a554b4f327448a94ac6785c4f89
Dax Raad <d@ironbay.co>
2025-11-23 14:32:41 -0500

    token

--- infra/enterprise.ts
+++ infra/enterprise.ts
@@ -1,22 +1,6 @@
 import { domain } from "./stage"
 
 const storage = new sst.cloudflare.Bucket("EnterpriseStorage")
-const token = new cloudflare.ApiToken("EnterpriseStorageToken", {
-  name: `${$app.name}-${$app.stage}-enterprise-storage`,
-  policies: [
-    {
-      effect: "allow",
-      resources: {
-        "com.cloudflare.api.account.*": "*",
-      },
-      permissionGroups: [
-        {
-          id: "c8d07a38f1654800b34e33e59b4e8f41",
-        },
-      ],
-    },
-  ],
-})
 
 const enterprise = new sst.cloudflare.x.SolidStart("Enterprise", {
   domain: "enterprise." + domain,
