339b6a3f46037d16526a7d7d901affe9d7d9b7d4
Dax Raad <d@ironbay.co>
2025-11-23 15:00:29 -0500

    ci: remove log

--- infra/enterprise.ts
+++ infra/enterprise.ts
@@ -3,7 +3,6 @@ import { domain } from "./stage"
 
 const storage = new sst.cloudflare.Bucket("EnterpriseStorage")
 
-console.log(SECRET.R2AccessKey.value, SECRET.R2SecretKey.value)
 const enterprise = new sst.cloudflare.x.SolidStart("Enterprise", {
   domain: "enterprise." + domain,
   path: "packages/enterprise",
