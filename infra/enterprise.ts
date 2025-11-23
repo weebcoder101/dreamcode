8905678945169dc45b727fb219b8e80e2d739537
Dax Raad <d@ironbay.co>
2025-11-23 14:36:25 -0500

    ci: secret

--- infra/enterprise.ts
+++ infra/enterprise.ts
@@ -1,3 +1,4 @@
+import { SECRET } from "./secret"
 import { domain } from "./stage"
 
 const storage = new sst.cloudflare.Bucket("EnterpriseStorage")
@@ -7,8 +8,8 @@ const enterprise = new sst.cloudflare.x.SolidStart("Enterprise", {
   environment: {
     OPENCODE_STORAGE_ADAPTER: "r2",
     OPENCODE_STORAGE_ACCOUNT_ID: sst.cloudflare.DEFAULT_ACCOUNT_ID,
-    OPENCODE_STORAGE_ACCESS_KEY_ID: "---",
-    OPENCODE_STORAGE_SECRET_ACCESS_KEY: "---",
+    OPENCODE_STORAGE_ACCESS_KEY_ID: SECRET.R2AccessKey.value,
+    OPENCODE_STORAGE_SECRET_ACCESS_KEY: SECRET.R2SecretKey.value,
     OPENCODE_STORAGE_BUCKET: storage.name,
   },
 })
