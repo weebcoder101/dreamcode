49f3bd2b273c5632ba4afc07ef062efbad0dd767
Dax Raad <d@ironbay.co>
2025-12-07 15:52:27 -0500

    remove outdated SDKs

--- packages/sdk/python/examples/file_status.py
+++
@@ -1,6 +0,0 @@
-from opencode_ai import OpenCodeClient
-
-client = OpenCodeClient()
-files = client.file_status() or []
-for f in files:
-    print(f.path, f.type)
--- packages/sdk/python/examples/session_list.py
+++
@@ -1,4 +0,0 @@
-from opencode_ai import OpenCodeClient
-
-client = OpenCodeClient()
-print([s.id for s in client.list_sessions() or []])
