1180ac9936d3b6c2dae58dac45bc4bf58625985d
Aiden Cline <aidenpcline@gmail.com>
2025-12-07 12:17:40 -0600

    ci: fix format

--- script/format.ts
+++ script/format.ts
@@ -5,9 +5,10 @@ import { $ } from "bun"
 await $`bun run prettier --ignore-unknown --write .`
 
 if (process.env["CI"] && (await $`git status --porcelain`.text())) {
+  const branch = process.env["GITHUB_HEAD_REF"] || process.env["GITHUB_REF_NAME"]
   await $`git config --local user.email "action@github.com"`
   await $`git config --local user.name "GitHub Action"`
   await $`git add -A`
   await $`git commit -m "chore: format code"`
-  await $`git push --no-verify`
+  await $`git push origin HEAD:${branch} --no-verify`
 }
