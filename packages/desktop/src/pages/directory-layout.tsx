37ad6f535158887bb7fb80a31af15c71d4761018
Adam <2363879+adamdotdevin@users.noreply.github.com>
2025-12-09 11:08:55 -0600

    wip(desktop): progress

--- packages/desktop/src/pages/directory-layout.tsx
+++ packages/desktop/src/pages/directory-layout.tsx
@@ -1,32 +1,31 @@
-import { createMemo, type ParentProps } from "solid-js"
+import { createMemo, Show, type ParentProps } from "solid-js"
 import { useParams } from "@solidjs/router"
 import { SDKProvider } from "@/context/sdk"
 import { SyncProvider, useSync } from "@/context/sync"
 import { LocalProvider } from "@/context/local"
-import { useGlobalSync } from "@/context/global-sync"
 import { base64Decode } from "@opencode-ai/util/encode"
 import { DataProvider } from "@opencode-ai/ui/context"
 import { iife } from "@opencode-ai/util/iife"
 
 export default function Layout(props: ParentProps) {
   const params = useParams()
-  const sync = useGlobalSync()
   const directory = createMemo(() => {
-    const decoded = base64Decode(params.dir!)
-    return sync.data.projects.find((x) => x.worktree === decoded)?.worktree ?? "/"
+    return base64Decode(params.dir!)
   })
   return (
-    <SDKProvider directory={directory()}>
-      <SyncProvider>
-        {iife(() => {
-          const sync = useSync()
-          return (
-            <DataProvider data={sync.data} directory={directory()}>
-              <LocalProvider>{props.children}</LocalProvider>
-            </DataProvider>
-          )
-        })}
-      </SyncProvider>
-    </SDKProvider>
+    <Show when={params.dir} keyed>
+      <SDKProvider directory={directory()}>
+        <SyncProvider>
+          {iife(() => {
+            const sync = useSync()
+            return (
+              <DataProvider data={sync.data} directory={directory()}>
+                <LocalProvider>{props.children}</LocalProvider>
+              </DataProvider>
+            )
+          })}
+        </SyncProvider>
+      </SDKProvider>
+    </Show>
   )
 }
