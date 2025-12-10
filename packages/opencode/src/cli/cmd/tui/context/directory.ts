e74ce9021c49f18a655c8e41819c977b1bd5479e
Aiden Cline <aidenpcline@gmail.com>
2025-12-10 16:01:10 -0600

    tweak: small fix

--- packages/opencode/src/cli/cmd/tui/context/directory.ts
+++ packages/opencode/src/cli/cmd/tui/context/directory.ts
@@ -5,7 +5,7 @@ import { Global } from "@/global"
 export function useDirectory() {
   const sync = useSync()
   return createMemo(() => {
-    const directory = sync.data.path.directory ?? process.cwd()
+    const directory = sync.data.path.directory || process.cwd()
     const result = directory.replace(Global.Path.home, "~")
     if (sync.data.vcs?.branch) return result + ":" + sync.data.vcs.branch
     return result
