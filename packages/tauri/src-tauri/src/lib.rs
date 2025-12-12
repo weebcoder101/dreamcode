9df01966248344f70631ecd12b2c49e330988519
Brendan Allan <brendonovich@outlook.com>
2025-12-12 16:58:37 +0800

    tauri: initialise store and window-state plugins

--- packages/tauri/src-tauri/src/lib.rs
+++ packages/tauri/src-tauri/src/lib.rs
@@ -107,6 +107,8 @@ pub fn run() {
     let updater_enabled = option_env!("TAURI_SIGNING_PRIVATE_KEY").is_some();
 
     let mut builder = tauri::Builder::default()
+        .plugin(tauri_plugin_window_state::Builder::new().build())
+        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_process::init())
