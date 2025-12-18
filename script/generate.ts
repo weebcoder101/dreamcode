24b7e726acdf50733e689f92405573a7c8bfe28c
Aiden Cline <aidenpcline@gmail.com>
2025-12-18 14:39:44 -0600

    ci: fix file perm

--- script/generate.ts
+++ script/generate.ts
@@ -2,11 +2,8 @@
 
 import { $ } from "bun"
 
-// Build SDK
 await $`bun ./packages/sdk/js/script/build.ts`
 
-// Generate openapi.json
 await $`bun dev generate > ../sdk/openapi.json`.cwd("packages/opencode")
 
-// Format
 await $`./script/format.ts`
