54423409d3154741fcf4a28e1fbfdcf5229d2855
Aiden Cline <aidenpcline@gmail.com>
2025-12-18 14:33:40 -0600

    ci: handle case where generate.yml fails better

--- script/format.ts
+++ script/format.ts
@@ -3,12 +3,3 @@
 import { $ } from "bun"
 
 await $`bun run prettier --ignore-unknown --write .`
-
-if (process.env["CI"] && (await $`git status --porcelain`.text())) {
-  await $`git config --local user.email "action@github.com"`
-  await $`git config --local user.name "GitHub Action"`
-  await $`git add -A`
-  await $`git commit -m "chore: format code"`
-  const branch = process.env["PUSH_BRANCH"]
-  await $`git push origin HEAD:${branch} --no-verify`
-}
