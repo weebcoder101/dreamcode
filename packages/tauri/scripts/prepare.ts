42b38b9e515c148ea73fafdf48b98bec427a93bf
GitHub Action <action@github.com>
2025-12-22 10:06:29 +0000

    chore: generate

--- packages/tauri/scripts/prepare.ts
+++ packages/tauri/scripts/prepare.ts
@@ -8,6 +8,8 @@ const sidecarConfig = getCurrentSidecar()
 const dir = "src-tauri/target/opencode-binaries"
 
 await $`mkdir -p ${dir}`
-await $`gh run download ${Bun.env.GITHUB_RUN_ID} -n opencode-cli`.cwd(dir);
+await $`gh run download ${Bun.env.GITHUB_RUN_ID} -n opencode-cli`.cwd(dir)
 
-await copyBinaryToSidecarFolder(`${dir}/${sidecarConfig.ocBinary}/bin/opencode${process.platform === "win32" ? ".exe" : ""}`)
+await copyBinaryToSidecarFolder(
+  `${dir}/${sidecarConfig.ocBinary}/bin/opencode${process.platform === "win32" ? ".exe" : ""}`,
+)
