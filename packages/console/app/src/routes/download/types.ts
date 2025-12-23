aa11221b2bfa4fb944dd06714f49a0a2217e515d
GitHub Action <action@github.com>
2025-12-23 05:53:02 +0000

    chore: generate

--- packages/console/app/src/routes/download/types.ts
+++ packages/console/app/src/routes/download/types.ts
@@ -1,1 +1,4 @@
-export type DownloadPlatform = `darwin-${"x64" | "aarch64"}-dmg` | "windows-x64-nsis" | `linux-x64-${"deb" | "rpm" | "appimage"}`
+export type DownloadPlatform =
+  | `darwin-${"x64" | "aarch64"}-dmg`
+  | "windows-x64-nsis"
+  | `linux-x64-${"deb" | "rpm" | "appimage"}`
