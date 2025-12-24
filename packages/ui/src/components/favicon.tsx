125768df762cda569965d2f070f6598dd17b8ba0
Ryan Vogel <ryan@mandarin3d.com>
2025-12-24 06:17:13 -0500

    fix: remove SVG favicon to improve SEO (#5755)

--- packages/ui/src/components/favicon.tsx
+++ packages/ui/src/components/favicon.tsx
@@ -4,7 +4,6 @@ export const Favicon = () => {
   return (
     <>
       <Link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-      <Link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       <Link rel="shortcut icon" href="/favicon.ico" />
       <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <Link rel="manifest" href="/site.webmanifest" />
