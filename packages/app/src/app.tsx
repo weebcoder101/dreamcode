ab01c536f49e5b5879fed8f3430ed597ddccac08
Adam <2363879+adamdotdevin@users.noreply.github.com>
2025-12-29 10:42:08 -0600

    fix(desktop): performance/jankiness

--- packages/app/src/app.tsx
+++ packages/app/src/app.tsx
@@ -69,7 +69,7 @@ export function App() {
                               <Route
                                 path="/session/:id?"
                                 component={(p) => (
-                                  <Show when={p.params.id || true} keyed>
+                                  <Show when={p.params.id ?? "new"} keyed>
                                     <TerminalProvider>
                                       <PromptProvider>
                                         <Session />
