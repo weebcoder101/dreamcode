4959c78417c10b0b6c1a6b65cee1d49ba9efe932
Kit Langton <kit.langton@gmail.com>
2026-03-17 21:59:54 -0400

    refactor(truncation): effectify TruncateService, delete Scheduler (#17957)

--- packages/opencode/src/effect/instances.ts
+++ packages/opencode/src/effect/instances.ts
@@ -3,7 +3,7 @@ import { FileService } from "@/file"
 import { FileTimeService } from "@/file/time"
 import { FileWatcherService } from "@/file/watcher"
 import { FormatService } from "@/format"
-import { PermissionService } from "@/permission/service"
+import { PermissionEffect } from "@/permission/service"
 import { Instance } from "@/project/instance"
 import { VcsService } from "@/project/vcs"
 import { ProviderAuthService } from "@/provider/auth-service"
@@ -17,7 +17,7 @@ export { InstanceContext } from "./instance-context"
 
 export type InstanceServices =
   | QuestionService
-  | PermissionService
+  | PermissionEffect.Service
   | ProviderAuthService
   | FileWatcherService
   | VcsService
@@ -37,7 +37,7 @@ function lookup(_key: string) {
   const ctx = Layer.sync(InstanceContext, () => InstanceContext.of(Instance.current))
   return Layer.mergeAll(
     Layer.fresh(QuestionService.layer),
-    Layer.fresh(PermissionService.layer),
+    Layer.fresh(PermissionEffect.layer),
     Layer.fresh(ProviderAuthService.layer),
     Layer.fresh(FileWatcherService.layer).pipe(Layer.orDie),
     Layer.fresh(VcsService.layer),
@@ -66,9 +66,5 @@ export class Instances extends ServiceMap.Service<Instances, LayerMap.LayerMap<s
 
   static get(directory: string): Layer.Layer<InstanceServices, never, Instances> {
     return Layer.unwrap(Instances.use((map) => Effect.succeed(map.get(directory))))
-  }
-
-  static invalidate(directory: string): Effect.Effect<void, never, Instances> {
-    return Instances.use((map) => map.invalidate(directory))
   }
 }
