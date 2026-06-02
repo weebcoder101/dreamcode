6d196a4af8dc000142f1c4d47ad9018b170ed1df
Shoubhit Dash <shoubhit2005@gmail.com>
2026-06-02 19:55:11 +0530

    feat(core): add flagged project references (#30414)

--- packages/core/test/location-layer.test.ts
+++ packages/core/test/location-layer.test.ts
@@ -16,6 +16,7 @@ import { Global } from "../src/global"
 import { ModelsDev } from "../src/models-dev"
 import { Npm } from "../src/npm"
 import { Project } from "../src/project"
+import { ProjectReference } from "../src/project-reference"
 
 const it = testEffect(
   LocationServiceMap.layer.pipe(
@@ -53,6 +54,7 @@ describe("LocationServiceMap", () => {
           const update = (directory: string) =>
             Effect.gen(function* () {
               yield* PluginBoot.Service.use((boot) => boot.wait())
+              yield* ProjectReference.Service
               const catalog = yield* Catalog.Service
               const transform = yield* catalog.transform()
               yield* transform((editor) => editor.provider.update(ProviderV2.ID.make("test"), () => {}))
