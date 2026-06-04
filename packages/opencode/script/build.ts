ca9e5b8a9a078ca8c6f14516e6ecddaf8c22d36a
Sebastian <hasta84@gmail.com>
2026-06-04 22:05:59 +0200

    upgrade opentui to 0.3.2 (#30748)

--- packages/opencode/script/build.ts
+++ packages/opencode/script/build.ts
@@ -192,6 +192,7 @@ for (const item of targets) {
       OPENCODE_WORKER_PATH: workerPath,
       OPENCODE_CHANNEL: `'${Script.channel}'`,
       OPENCODE_LIBC: item.os === "linux" ? `'${item.abi ?? "glibc"}'` : "",
+      ...(item.os === "linux" ? { "process.env.OPENTUI_LIBC": JSON.stringify(item.abi ?? "glibc") } : {}),
     },
   })
 
@@ -217,6 +218,7 @@ for (const item of targets) {
         preferUnplugged: true,
         os: [item.os],
         cpu: [item.arch],
+        ...(item.abi ? { libc: [item.abi] } : {}),
       },
       null,
       2,
